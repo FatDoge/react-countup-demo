760a58bca5e69f420cc11c233c16dbf41cd957fa
fatdoge <704274491@qq.com>
2019-09-27 17:29:59 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import CountUp from 'react-countup';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    number: 0,
+    clock: null,
+  }
+
+  componentDidMount() {
+    this.renderNumber();
+    const clock = setInterval(this.renderNumber, 5000);
+    this.setState({
+      clock
+    })
+  }
+
+  componentWillUnmount() {
+    const { clock } = this.state;
+    clearInterval(clock)
+  }
+
+  renderNumber = () => {
+    const number = Math.floor(Math.random() * 10000 + 1)
+    this.setState({
+      number
+    })
+  }
+
+
+  render() {
+    const { number } = this.state;
+    return (
+      <div className='container'>
+        <CountUp className='middle' end={number} preserveValue/>
+      </div>
+    );
+  }
+
 }
 
 export default App;
